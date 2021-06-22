ce39af48db4c34afed689bc7263e086e40cd25af
Anthony Johnson <theanthonyjohnson@gmail.com>
2021-06-22 17:48:41 -0500

    finished lab 11

--- src/App.js
+++ src/App.js
@@ -11,8 +11,22 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import axios from 'axios';
 
 class App extends React.Component {
+  makeRequest = async () => {
+    const { getIdTokenClaims } = this.props.auth0;
+    let tokenClaims = await getIdTokenClaims();
+    const jwt = tokenClaims.__raw;
+
+    const config = {
+      headers: {"Authorization" : `Bearer ${jwt}`}
+    };
+
+    const serverResponse = await axios.get('http://localhost:3001/test-login', config);
+
+    console.log(serverResponse);
+  }
 
   render() {
     console.log('app', this.props);
@@ -23,12 +37,12 @@ class App extends React.Component {
           <IsLoadingAndError>
             <Header />
             <Switch>
-              <Route exact path="./">
+              <Route exact path="/">
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
                 {this.props.auth0.isAuthenticated ?  <MyFavoriteBooks /> : <Login />}
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-              <Route exact path="./profile">
+              <Route exact path="/profile">
               {this.props.auth0.isAuthenticated ?  <Profile /> : <h1>Hey there!</h1>}
               </Route>
             </Switch>
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <Auth0Provider
   domain="dev-jjw40wxd.us.auth0.com"
   clientId="AeZrD6YZKuP1G1sgjkgu4aBS2jYCO0N2"
-  redirectUri="http://localhost:3000/profile"
+  redirectUri="http://localhost:3000/"
 >
   <App />
 </Auth0Provider>,
